c822f1590d12eff0d125a7abb57aad826195b773
qianxi_real <qianxi@ualberta.ca>
2022-11-07 16:02:54 -0700

    minor

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import pandas as pd
 from sklearn.metrics import classification_report
 import os
 import matplotlib.pyplot as plt
+import tensorflow_model_optimization as tfmot
 
 ssl._create_default_https_context = ssl._create_unverified_context
 trainset_identifier = 20000
@@ -27,8 +28,10 @@ train_dir = "./nov7data/train"
 validation_dir = "./nov7data/val"
 
 cur_path = "/Users/qianxi/Desktop/Leon/2022-2024/2022fall/644/project/code/"
-
-
+timestamp = datetime.now()
+dt_string = timestamp.strftime("%Y%m%d")
+full_store_path = f'644model/{dt_string}_{trainset_identifier}train_{testval_set_identifier}valtest_pretrained_{initial_epochs}train_{fine_tune_epochs}finetune/'
+full_path = cur_path + full_store_path
 
 
 # Load train, test, validation set.
@@ -37,7 +40,8 @@ train_dataset = tf.keras.utils.image_dataset_from_directory(train_dir,
                                                             label_mode="int",
                                                             batch_size=BATCH_SIZE,
                                                             image_size=IMG_SIZE)
-                                                    
+
+train_batches = tf.data.experimental.cardinality(train_dataset)
 validation_dataset = tf.keras.utils.image_dataset_from_directory(validation_dir,
                                                                  shuffle=True,
                                                                  label_mode="int",
@@ -53,6 +57,7 @@ AUTOTUNE = tf.data.AUTOTUNE
 
 # Use buffered prefetching to load images from disk without having I/O become blocking.
 train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
+
 validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
 test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
 
@@ -96,16 +101,54 @@ def compose_model(base_model):
     
     return model
 
+def prune_model(base_model):
+    # Create the base model from the pre-trained model MobileNet V2.
+    prune_low_magnitude = tfmot.sparsity.keras.prune_low_magnitude
+    pruning_params = {
+      'pruning_schedule': tfmot.sparsity.keras.PolynomialDecay(initial_sparsity=0.50,
+                                                               final_sparsity=0.80,
+                                                               begin_step=0,
+                                                               end_step=train_batches)
+}
+    base_model.trainable = False
+    image_batch, label_batch = next(iter(train_dataset))
+    feature_batch = base_model(image_batch)
+    print(feature_batch.shape)
+    # Add a classification head.
+    global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+    feature_batch_average = global_average_layer(feature_batch)
+    print(feature_batch_average.shape)
+
+    prediction_layer = tf.keras.layers.Dense(1)
+    prediction_batch = prediction_layer(feature_batch_average)
+    print(prediction_batch.shape)
+
+    inputs = tf.keras.Input(shape=(224, 224, 3))
+    x = data_augmentation(inputs)
+    x = preprocess_input(x)
+    x = base_model(x, training=False)
+    x = global_average_layer(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+    outputs = prediction_layer(x)
+    model = tf.keras.Model(inputs, outputs)
+    
+    
+    return model
+
 def train_model_with_finetune(base_model, model,base_lr,initial_epochs, fine_tune_epochs):
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_lr),
               loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
               metrics=['accuracy',f1_m,precision_m, recall_m])
     print(model.summary())
     earlystop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', verbose=1, mode='min',patience=3)
+    # Create a callback that saves the model's weights
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=full_path+"cp.ckpt",
+                                                 save_weights_only=True,
+                                                 verbose=1)
 
     history = model.fit(train_dataset,
                     epochs=initial_epochs,
-                    validation_data=validation_dataset,callbacks=[earlystop] )
+                    validation_data=validation_dataset,callbacks=[earlystop,cp_callback] )
 
     acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
@@ -156,11 +199,9 @@ print('Test accuracy :', accuracy)
 
 report = report_confusion_matrix(model, test_dataset)
 print(report)
-timestamp = datetime.now()
-dt_string = timestamp.strftime("%Y%m%d")
-full_store_path = f'644model/{dt_string}_{trainset_identifier}train_{testval_set_identifier}valtest_pretrained_{initial_epochs}train_{fine_tune_epochs}finetune/'
 
-full_path = cur_path + full_store_path
+
+
 print(full_path)
 
 
